b3a5d313ee17c58a2b2f9e60fe354a8e0eb6f738
sazzad7299 <sazzadurrahman580@gmail.com>
2022-02-05 21:56:41 +0600

    update

--- themes/frontend/views/home.blade.php
+++ themes/frontend/views/home.blade.php
@@ -20,4 +20,23 @@
             </div>
         </div>
     </div>
+    <div class="container">
+        <div class="row">
+            @for($i=0;$i<=10;$i++)
+            <div class="col-md-4">
+                <div class="card m-4">
+                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-danger">Buy</a> <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            @endfor
+        </div>
+    </div>
+    
+    
+    
 @endsection
